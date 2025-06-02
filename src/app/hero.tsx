0a65fbeceeea5cbe422eec63f52029d099d7963e
maxgorm <maxgorm@umich.edu>
2025-06-02 19:57:59 -0400

    fixes

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { Button, Typography } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
 
 
 function Hero() {
@@ -10,18 +10,17 @@ function Hero() {
       <header className="grid !min-h-[49rem] bg-carsnap-blue px-8">
         <div className="container mx-auto mt-32 grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-2">
           <div className="col-span-1">
-            <Typography variant="h1" color="white" className="mb-4">
+            <h1 className="mb-4 text-white text-4xl font-bold">
               Car Shopping <br /> Just Got Smarter
-            </Typography>
-            <Typography
-              variant="lead"
-              className="mb-7 !text-white md:pr-16 xl:pr-28"
+            </h1>
+            <p
+              className="mb-7 text-white md:pr-16 xl:pr-28 text-xl"
             >
-              See a car you like? Simply take a picture.<br></br> We&apos;ll handle the rest
-            </Typography>
-            <Typography className="mb-4" color="white" variant="h6">
+              See a car you like? Simply take a picture.<br></br> We'll handle the rest
+            </p>
+            <h6 className="mb-4 text-white text-base font-semibold">
               Get the app
-            </Typography>
+            </h6>
             <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
               <Button
                 size="lg"
@@ -66,18 +65,17 @@ function Hero() {
       </header>
       <div className="mx-8 lg:mx-16 -mt-24 rounded-xl bg-white p-5 md:p-14 shadow-md">
         <div>
-          <Typography variant="h3" className="mb-3 text-carsnap-blue">
+          <h3 className="mb-3 text-carsnap-blue text-2xl font-bold">
             AI Enhanced
-          </Typography>
-          <Typography
-            variant="paragraph"
-            className="font-normal !text-gray-500 lg:w-5/12"
+          </h3>
+          <p
+            className="font-normal text-gray-500 lg:w-5/12"
           >
             Gone are the days of spotting a car you like and spending
             hours searching for it online. With CarSnap you can simply take a
-            picture and we&apos;ll show you listings, specifications, reviews
+            picture and we'll show you listings, specifications, reviews
             and more!
-          </Typography>
+          </p>
         </div>
       </div>
     </div>
